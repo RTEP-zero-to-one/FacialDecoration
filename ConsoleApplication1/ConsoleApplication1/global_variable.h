839bea2f1ac9b43d047a9b4254a551902e64db5b
Tianze Zhang <1730420225@qq.com>
2022-02-04 16:09:23 +0000

    Add some facial features

--- ConsoleApplication1/ConsoleApplication1/global_variable.h
+++ ConsoleApplication1/ConsoleApplication1/global_variable.h
@@ -1,7 +1,13 @@
 #pragma once
 #include <opencv2/opencv.hpp>
-#include <opencv2/ml.hpp>
+#include<opencv2\ml.hpp>
 using namespace cv;
-extern CascadeClassifier cascade;
+extern CascadeClassifier cascade_face;
+extern CascadeClassifier cascade_nose;
+extern CascadeClassifier cascade_mouth;
+extern CascadeClassifier cascade_eye;
 extern Mat frame;
 #define FACE_XML "face_detect.xml"
+#define NOSE_XML "nose_detect.xml"
+#define EYE_XML "eye_detect.xml"
+#define MOUTH_XML "mouth_detect.xml"
---
+++ ConsoleApplication1/ConsoleApplication1/main.cpp
@@ -0,0 +1,39 @@
+﻿// ConsoleApplication1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include "global_variable.h"
+#include "face_detect.h"
+
+using namespace std;
+using namespace cv;
+CascadeClassifier cascade_face;
+
+Mat frame, imageGray;
+int main()
+{
+	if (!cascade_face.load(FACE_XML))
+	{
+		printf("脸部级联分类器错误，可能未找到文件，拷贝该文件到工程目录下！\n");
+	}
+	VideoCapture capture;
+	capture.open(0);
+	if (false == capture.isOpened())
+	{
+		cout << "摄像头没有打开！！！" << endl;
+		return -1;
+	}
+	
+	while (true) {
+		capture >> frame;
+		Rect faceRect;
+		faceDetect(frame, cascade_face, faceRect);
+		rectangle(frame, faceRect, Scalar(0, 255, 0), 2);
+		imshow("原图像", frame);
+		waitKey(1);
+	}
+	capture.release();
+	return 0;
+}
+
