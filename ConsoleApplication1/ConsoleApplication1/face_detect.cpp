0a945270ade063bdcae33c65640c666f5fd20605
Tianze Zhang <1730420225@qq.com>
2022-02-11 18:38:33 +0000

    Add some methods to detect nose mouth and eyes

--- ConsoleApplication1/ConsoleApplication1/face_detect.cpp
+++ ConsoleApplication1/ConsoleApplication1/face_detect.cpp
@@ -1,12 +1,11 @@
 # include "face_detect.h"
-bool faceDetect(Mat& src, CascadeClassifier& cascade,  Rect& faceRect)
+bool faceDetect(const Mat& src, CascadeClassifier& cascade,  Rect& faceRect)
 {
-	int scale = 5;
 	Mat imgGray;
 	Mat srcTemp = src.clone();
 	cvtColor(srcTemp, imgGray, COLOR_BGR2GRAY);
 	equalizeHist(imgGray, imgGray);
-	Mat imgSmall(cvRound(src.rows / scale), cvRound(src.cols / scale), CV_8UC1);
+	Mat imgSmall(cvRound(src.rows /MINISCALE), cvRound(src.cols / MINISCALE), CV_8UC1);
 	resize(imgGray, imgSmall, imgSmall.size(), 0, 0, INTER_LINEAR);
 	vector<Rect>faces;
 	Rect maxFace;
@@ -30,11 +29,126 @@ bool faceDetect(Mat& src, CascadeClassifier& cascade,  Rect& faceRect)
 	}
 	maxFace = faces[maxIndex];
 	Point p = maxFace.tl();
-	int w = scale * maxFace.width;
-	int h = scale * maxFace.height;
-	int x = scale * (p.x);
-	int y = scale * (p.y);
+	int w = MINISCALE * maxFace.width;
+	int h = MINISCALE * maxFace.height;
+	int x = MINISCALE * (p.x);
+	int y = MINISCALE * (p.y);
 	Rect faceLocation(x, y, w, h);
 	faceRect = faceLocation;
+	return 1;
+}
+bool mouthDetect(const Mat& src, CascadeClassifier& cascade, Rect& mouthRect)
+{
+	Mat imgGray;
+	Mat srcTemp = src.clone();
+	cvtColor(srcTemp, imgGray, COLOR_BGR2GRAY);
+	equalizeHist(imgGray, imgGray);
+	Mat imgSmall(cvRound(src.rows / MINISCALE), cvRound(src.cols / MINISCALE), CV_8UC1);
+	resize(imgGray, imgSmall, imgSmall.size(), 0, 0, INTER_LINEAR);
+	vector<Rect> mouths;
+	Rect maxMouth;
+	cascade.detectMultiScale(imgSmall, mouths, 1.1, 2, 0, Size(30, 30));
+	if (mouths.size() == 0)
+	{
+		return false;
+	}
+
+	double maxArea = 0;
+	int maxIndex = 0;
+
+	for (int i = 0; i < mouths.size(); i++)
+	{
+		double area = mouths[i].area();
+		if (area > maxArea)
+		{
+			maxIndex = i;
+			maxArea = area;
+		}
+	}
+	maxMouth = mouths[maxIndex];
+	Point p = maxMouth.tl();
+	int w = MINISCALE * maxMouth.width;
+	int h = MINISCALE * maxMouth.height;
+	int x = MINISCALE * (p.x);
+	int y = MINISCALE * (p.y);
+	Rect mouthLocation(x, y, w, h);
+	mouthRect = mouthLocation;
+	return 1;
+}
+bool eyeDetect(const Mat& src, CascadeClassifier& cascade, Rect& eyeRect)
+{
+	Mat imgGray;
+	Mat srcTemp = src.clone();
+	cvtColor(srcTemp, imgGray, COLOR_BGR2GRAY);
+	equalizeHist(imgGray, imgGray);
+	Mat imgSmall(cvRound(src.rows / MINISCALE), cvRound(src.cols / MINISCALE), CV_8UC1);
+	resize(imgGray, imgSmall, imgSmall.size(), 0, 0, INTER_LINEAR);
+	vector<Rect> eyes;
+	Rect maxEye;
+	cascade.detectMultiScale(imgSmall, eyes, 1.1, 2, 0, Size(30, 30));
+	if (eyes.size() == 0)
+	{
+		cout << "zhaobudao" << endl;
+		return false;
+	}
+
+	double maxArea = 0;
+	int maxIndex = 0;
+
+	for (int i = 0; i < eyes.size(); i++)
+	{
+		double area = eyes[i].area();
+		if (area > maxArea)
+		{
+			maxIndex = i;
+			maxArea = area;
+		}
+	}
+	maxEye = eyes[maxIndex];
+	Point p = maxEye.tl();
+	int w = MINISCALE * maxEye.width;
+	int h = MINISCALE * maxEye.height;
+	int x = MINISCALE * (p.x);
+	int y = MINISCALE * (p.y);
+	Rect eyeLocation(x, y, w, h);
+	eyeRect = eyeLocation;
+	return 1;
+}
+bool noseDetect(const Mat& src, CascadeClassifier& cascade, Rect& noseRect)
+{
+	Mat imgGray;
+	Mat srcTemp = src.clone();
+	cvtColor(srcTemp, imgGray, COLOR_BGR2GRAY);
+	equalizeHist(imgGray, imgGray);
+	Mat imgSmall(cvRound(src.rows / MINISCALE), cvRound(src.cols / MINISCALE), CV_8UC1);
+	resize(imgGray, imgSmall, imgSmall.size(), 0, 0, INTER_LINEAR);
+	vector<Rect> noses;
+	Rect maxNose;
+	cascade.detectMultiScale(imgSmall, noses, 1.1, 2, 0, Size(30, 30));
+	if (noses.size() == 0)
+	{
+		return 0;
+	}
+
+	double maxArea = 0;
+	int maxIndex = 0;
+
+	for (int i = 0; i < noses.size(); i++)
+	{
+		double area = noses[i].area();
+		if (area > maxArea)
+		{
+			maxIndex = i;
+			maxArea = area;
+		}
+	}
+	maxNose = noses[maxIndex];
+	Point p = maxNose.tl();
+	int w = MINISCALE * maxNose.width;
+	int h = MINISCALE * maxNose.height;
+	int x = MINISCALE * (p.x);
+	int y = MINISCALE * (p.y);
+	Rect noseLocation(x, y, w, h);
+	noseRect = noseLocation;
 	return 1;
 }
--- ConsoleApplication1/ConsoleApplication1/face_detect.h
+++ ConsoleApplication1/ConsoleApplication1/face_detect.h
@@ -1,6 +1,7 @@
 #pragma once
-#include <opencv2/opencv.hpp>
-using namespace std;
-using namespace cv;
-
-bool faceDetect(Mat& src, CascadeClassifier& cascade, Rect& faceRect);
+#include "global_variable.h"
+#include<opencv2/ml.hpp>
+bool faceDetect(const Mat& src, CascadeClassifier& cascade, Rect& faceRect);
+bool noseDetect(const Mat& src, CascadeClassifier& cascade, Rect& noseRect);
+bool eyeDetect(const Mat& src, CascadeClassifier& cascade, Rect& eyeRect);
+bool mouthDetect(const Mat& src,  CascadeClassifier& cascade, Rect& mouthRect);
