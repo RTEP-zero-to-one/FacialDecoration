4ee311060adee9cffd2b95274a60e97f3cbc8bbd
Tianze Zhang <1730420225@qq.com>
2022-02-11 18:39:33 +0000

    Add some test for the detection of eyes

--- ConsoleApplication1/ConsoleApplication1/main.cpp
+++ ConsoleApplication1/ConsoleApplication1/main.cpp
@@ -1,22 +1,20 @@
-﻿// ConsoleApplication1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-#include <opencv2/opencv.hpp>
-#include <iostream>
+﻿
 #include "global_variable.h"
 #include "face_detect.h"
-
-using namespace std;
+#include "cascade.h"
+#include <opencv2/opencv.hpp>
+#include<opencv2/ml.hpp>
+#include <iostream>
 using namespace cv;
-CascadeClassifier cascade_face;
-
-Mat frame, imageGray;
+using namespace std;
+Mat frame;
+Rect faceRect;
+Rect noseRect;
+Rect eyeRect;
+Rect mouthRect;
 int main()
 {
-	if (!cascade_face.load(FACE_XML))
-	{
-		printf("脸部级联分类器错误，可能未找到文件，拷贝该文件到工程目录下！\n");
-	}
+	loadCascade();
 	VideoCapture capture;
 	capture.open(0);
 	if (false == capture.isOpened())
@@ -24,12 +22,13 @@ int main()
 		cout << "摄像头没有打开！！！" << endl;
 		return -1;
 	}
-	
 	while (true) {
 		capture >> frame;
-		Rect faceRect;
-		faceDetect(frame, cascade_face, faceRect);
-		rectangle(frame, faceRect, Scalar(0, 255, 0), 2);
+		//faceDetect(frame, cascade_face, faceRect);
+		//mouthDetect(frame, cascade_mouth, mouthRect);
+		eyeDetect(frame, cascade_eye, eyeRect);
+		//noseDetect(frame, cascade_nose, noseRect);
+		rectangle(frame, eyeRect, Scalar(0, 255, 0), 2);
 		imshow("原图像", frame);
 		waitKey(1);
 	}
