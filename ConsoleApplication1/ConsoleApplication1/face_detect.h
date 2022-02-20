7b6ef5cef513b4cfb55fd0963d4f21f7675d0293
Tianze Zhang <1730420225@qq.com>
2022-02-20 20:15:39 +0000

    Add getAngle

--- ConsoleApplication1/ConsoleApplication1/face_detect.h
+++ ConsoleApplication1/ConsoleApplication1/face_detect.h
@@ -1,7 +1,9 @@
 #pragma once
 #include "global_variable.h"
 #include<opencv2/ml.hpp>
+
 bool faceDetect(const Mat& src, CascadeClassifier& cascade, Rect& faceRect);
 bool noseDetect(const Mat& src, CascadeClassifier& cascade, Rect& noseRect);
-bool eyeDetect(const Mat& src, CascadeClassifier& cascade, Rect& eyeRect);
+bool eyeDetect(const Mat& src, CascadeClassifier& cascade, vector<Rect>& eyeRect);
 bool mouthDetect(const Mat& src,  CascadeClassifier& cascade, Rect& mouthRect);
+bool getAngle(const Mat& src, vector<Rect>& eyeRect);
