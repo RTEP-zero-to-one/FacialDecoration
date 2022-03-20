592a36d8c00ab48f8ee8bf7a3845e80f111a0958
Tianze Zhang <1730420225@qq.com>
2022-03-20 20:01:54 +0000

    complete the hat stick function

--- ConsoleApplication1/ConsoleApplication1/img_process.cpp
+++ ConsoleApplication1/ConsoleApplication1/img_process.cpp
@@ -5,7 +5,7 @@
 
 using namespace cv;
 using namespace std;
-
+/*
 Mat filter(Mat &src, int style_num) {
     if (!style_num)
         return src;
@@ -90,7 +90,16 @@ Mat filter(Mat &src, int style_num) {
             new_channels[1] = original_channels[1] * original_channels[1] / 255;
             new_channels[2] = original_channels[2] * original_channels[2] / 255;
             merge(new_channels, result);
+<<<<<<< Updated upstream
+=======
+        case SUMMER:
+            int increaseLookupTable = LookupTable([0, 64, 128, 256], [0, 80, 160, 256]);
+            int decreaseLookupTable = LookupTable([0, 64, 128, 256], [0, 50, 100, 256]);
+            new_channels[2] = cv2.LUT(original_channels[2], increaseLookupTable).astype(np.uint8);
+            new_channels[0] = cv2.LUT(original_channels[0], decreaseLookupTable).astype(np.uint8);
+            merge(new_channels, result);
+>>>>>>> Stashed changes
             break;
     }
     return result;
-}
+}*/
