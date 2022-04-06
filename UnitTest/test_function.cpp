02ec9ae5ac36ae29c28c219681c12088c20941ad
Paul <yingheting@gmail.com>
2022-04-06 20:22:32 +0100

    TEST all 4 filters passed

--- UnitTest/test_function.cpp
+++ UnitTest/test_function.cpp
@@ -1,15 +1,48 @@
 #include "../ImageProcess/global_variable.h"
-#include "../ImageProcess/img_process.h"
+#include "../ImageProcess/filter_process.h"
 #include "gtest/gtest.h"
 #include "iostream"
 
 
-TEST(OpenCVTest, imgprocess_OLDFASHION) {
+TEST(OpenCVTest, filter_OLDFASHION) {
     Mat src, result;
     src = imread("../assets/img.png", IMREAD_ANYCOLOR);
-    filter(src, FANTASY);
+    result = filter(src, OLDFASHION);
+//    imwrite("../assets/OLDFASHION.png", result);
 }
 //todo: other filters' test function
+
+TEST(OpenCVTest, filter_COMICBOOK) {
+    Mat src, result;
+    src = imread("../assets/img.png", IMREAD_ANYCOLOR);
+    result = filter(src, COMICBOOK);
+//    imwrite("../assets/COMICBOOK.png", result);
+
+}
+
+TEST(OpenCVTest, filter_FANTASY) {
+    Mat src, result;
+    src = imread("../assets/img.png", IMREAD_ANYCOLOR);
+    result = filter(src, FANTASY);
+//    imwrite("../assets/FANTASY.png", result);
+
+}
+
+TEST(OpenCVTest, filter_FREEZE) {
+    Mat src, result;
+    src = imread("../assets/img.png", IMREAD_ANYCOLOR);
+    result = filter(src, FREEZE);
+//    imwrite("../assets/FREEZE.png", result);
+
+}
+
+//TEST(OpenCVTest, filter_DARKTONE) {
+//    Mat src, result;
+//    src = imread("../assets/img.png", IMREAD_ANYCOLOR);
+//    result = filter(src, DARKTONE);
+//    imwrite("../assets/DARKTONE.png", result);
+//
+//}
 
 GTEST_API_ int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
