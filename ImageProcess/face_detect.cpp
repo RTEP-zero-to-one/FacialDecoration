b1a126333c2da964b061999ec493eec9f642319c
Paul <yingheting@gmail.com>
2022-04-12 00:22:28 +0100

    comment cancel

--- ImageProcess/face_detect.cpp
+++ ImageProcess/face_detect.cpp
@@ -231,7 +231,7 @@ Mat Detect::transform(const Mat& res) {
 		return res;
 	}
 	float eyeTan =(diffY*1.0)/(diffX*1.0);
-	cout << eyeTan << endl;
+//	cout << eyeTan << endl;
 	float eyeAngle = atan(eyeTan) * 180.0/ 3.14159*(-1);
 	Point2f center(res.cols / 2, res.rows / 2);//中心
 	Mat M = getRotationMatrix2D(center, eyeAngle, 1);//计算旋转的仿射变换矩阵 
