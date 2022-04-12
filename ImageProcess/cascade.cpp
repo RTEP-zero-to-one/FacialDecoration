de4ad094bf95506d8fe8db33ceed7f65a2fc3dcb
Paul <yingheting@gmail.com>
2022-04-12 23:54:14 +0100

    chinese conver to english to avoid encoding problem

--- ImageProcess/cascade.cpp
+++ ImageProcess/cascade.cpp
@@ -1,5 +1,3 @@
-
-
 #include "cascade.h"
 CascadeClassifier cascade_face;
 CascadeClassifier cascade_nose;
@@ -8,18 +6,18 @@ CascadeClassifier cascade_eye;
 void loadCascade() {
 	if (!cascade_face.load(FACE_XML))
 	{
-		printf("脸部级联分类器错误，可能未找到文件，拷贝该文件到工程目录下！\n");
+		printf("Face cascade xml file not found!\n");
 	}
 	if (!cascade_nose.load(NOSE_XML))
 	{
-		printf("鼻子级联分类器错误，可能未找到文件，拷贝该文件到工程目录下！\n");
+        printf("Nose cascade xml file not found!\n");
 	}
 	if (!cascade_eye.load(EYE_XML))
 	{
-		printf("眼睛级联分类器错误，可能未找到文件，拷贝该文件到工程目录下！\n");
+        printf("Eye cascade xml file not found!\n");
 	}
 	if (!cascade_mouth.load(MOUTH_XML))
 	{
-		printf("嘴巴级联分类器错误，可能未找到文件，拷贝该文件到工程目录下！\n");
+        printf("Mouth cascade xml file not found!\n");
 	}
 }
