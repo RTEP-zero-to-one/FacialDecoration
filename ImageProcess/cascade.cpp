d05769d77e5f16d06bd7c2203019df11ff9e902a
ZTZWILL <92398270+ZTZWILL@users.noreply.github.com>
2022-04-13 19:07:22 +0100

    change chinese to english

--- ImageProcess/cascade.cpp
+++ ImageProcess/cascade.cpp
@@ -8,18 +8,18 @@ CascadeClassifier cascade_eye;
 void loadCascade() {
 	if (!cascade_face.load(FACE_XML))
 	{
-		printf("脸部级联分类器错误，可能未找到文件，拷贝该文件到工程目录下！\n");
+		printf("face cascade filter faile!\n");
 	}
 	if (!cascade_nose.load(NOSE_XML))
 	{
-		printf("鼻子级联分类器错误，可能未找到文件，拷贝该文件到工程目录下！\n");
+		printf("nose cascade filter faile!\n");
 	}
 	if (!cascade_eye.load(EYE_XML))
 	{
-		printf("眼睛级联分类器错误，可能未找到文件，拷贝该文件到工程目录下！\n");
+		printf("eye cascade filter faile!\n");
 	}
 	if (!cascade_mouth.load(MOUTH_XML))
 	{
-		printf("嘴巴级联分类器错误，可能未找到文件，拷贝该文件到工程目录下！\n");
+		printf("mouth cascade filter faile!\n");
 	}
 }
