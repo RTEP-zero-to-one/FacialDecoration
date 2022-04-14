b401ad2d93848142573b71be842caa8577ae0d54
Paul <yingheting@gmail.com>
2022-04-14 20:03:43 +0100

    correct script

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ if [ ! -d "build" ];then
    mkdir -p "build"
 fi
 cd build
-cmake -B build -DCMAKE_BUILD_TYPE=Debug
-cmake --build build --config Debug
-cd build && make
+cmake -B . -DCMAKE_BUILD_TYPE=Debug
+cmake --build . --config Debug
+make
