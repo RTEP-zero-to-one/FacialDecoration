dab800d5ff1a664a1a4a3ed72ec5e37a204f63d8
Paul <yingheting@gmail.com>
2022-04-18 23:46:49 +0100

    update stand-alone application

--- run_application.sh
+++ run_application.sh
@@ -1,5 +1,5 @@
-wget https://raw.githubusercontent.com/RTEP-zero-to-one/FacialDecoration/main/FacialDecoration.tar.gz
-tar -xzvf FacialDecoration.tar.gz
-rm -rf FacialDecoration.tar.gz
-cd FacialDecoration
+wget https://raw.githubusercontent.com/RTEP-zero-to-one/FacialDecoration/main/FacialDecoration-SA.tar.gz
+tar -xzvf FacialDecoration-SA.tar.gz
+rm -rf FacialDecoration-SA.tar.gz
+cd FacialDecoration-SA
 ./FacialDecoration
